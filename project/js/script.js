99abe31e2438f89abe29060655100cad4cdaabc0
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-04-07 16:39:26 +0300

    create html css

---
+++ project/js/script.js
@@ -0,0 +1,57 @@
+// "use strict";
+
+const cards = document.querySelectorAll('.memo-card'); // Получаем массив элементов - карточек
+let hasFlipCard = false; // Перевернута ли карточка
+let boardLocked = false; //Запрет на кликание по другим карточкам пока открыты две одновременно
+let firstCard; // сохроняем в переменнные DOM элементы для дальнейшнго сравнения
+let secondCard;
+
+const flipCard = (e) => {
+    if(boardLocked) return;
+    const target = e.target.parentElement;
+
+    if(target === firstCard) return;  //если доска закрыта все действия прекращаются
+
+    target.classList.add("flip");
+    
+    if(!hasFlipCard) { //ПРоверяем какая была карточка
+        //First click
+        hasFlipCard = true;
+        firstCard = target;
+    }else{
+        //Second click
+        hasFlipCard = false;
+        secondCard = target;
+        checkForMatch(); // Проверка на совместимость, схожесть карточек (одинаковые ли)
+    }
+};
+//Chek for match
+function checkForMatch() {
+    const isEqual = firstCard.dataset.ikon === secondCard.dataset.ikon;
+    isEqual ? disableCards() : unflipCards(); // если тру остовляем карточки если false переворачиваем в исходное положение
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    boardLocked = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    },700);
+}
+
+function resetBoard() {
+    hasFlipCard = boardLocked = false;
+    firstCard = secondCard = null;
+}
+
+cards.forEach(card => {
+    card.addEventListener('click', flipCard);
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card.style.order = randomIndex;
+});
