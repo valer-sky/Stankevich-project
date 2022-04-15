f44c325fd135698d2a586fb80eb51c17909d91d5
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-04-15 15:36:23 +0300

    add array img

--- project/js/script.js
+++ project/js/script.js
@@ -5,72 +5,117 @@ toggleButton.addEventListener('click', function () {
 	navBar.classList.toggle('toggle');
 });
 
-const cards = document.querySelectorAll('.memo-card'); // Получаем массив элементов - карточек
-let hasFlipCard = false; // Перевернута ли карточка
-let boardLocked = false; //Запрет на кликание по другим карточкам пока открыты две одновременно
-let firstCard; // сохроняем в переменнные DOM элементы для дальнейшнго сравнения
-let secondCard;
-console.log(cards);
-
-
-const flipCard = (e) => {
-    if(boardLocked) return;
-    const target = e.target.parentElement;
-
-    if(target === firstCard) return;  //если доска закрыта все действия прекращаются
-
-    target.classList.add("flip");
+// let cardsContent = [
+//   {
     
-    if(!hasFlipCard) { //ПРоверяем какая была карточка
-        //First click
-        hasFlipCard = true;
-        firstCard = target;
-    }else{
-        //Second click
-        hasFlipCard = false;
-        secondCard = target;
-        checkForMatch(); // Проверка на совместимость, схожесть карточек (одинаковые ли)
-    }
-};
-//Chek for match
-function checkForMatch() {
-    const isEqual = firstCard.dataset.ikon === secondCard.dataset.ikon;
-    isEqual ? disableCards() : unflipCards(); // если тру остовляем карточки если false переворачиваем в исходное положение
-}
+//     img: 'images/1.png'
+//   },
+//   {
+    
+//     img: 'images/2.png'
+//   },
+//   {
+   
+//     img: 'images/3.png'
+//   },
+//   {
+    
+//     img: 'images/4.png'
+//   },
+//   {
+    
+//     img: 'images/5.png'
+//   },
+//   {
+    
+//     img: 'images/6.png'
+//   },
+//   {
+    
+//     img: 'images/7.png'
+//   },
+//   {
+//     img: 'images/8.png'
+//   },
+//   {
+    
+//     img: 'images/9.png'
+//   },
+//   {
+    
+//     img: 'images/10.png'
+//   },
+//   // {
+    
+  //   img: 'images/1.png'
+  // },
+  // {
+    
+  //   img: 'images/2.png'
+  // },
+  // {
+    
+  //   img: 'images/3.png'
+  // },
+  // {
+    
+  //   img: 'images/4.png'
+  // },
+  // {
+    
+  //   img: 'images/5.png'
+  // },
+  // {
+   
+  //   img: 'images/6.png'
+  // },
+  // {
+    
+  //   img: 'images/7.png'
+  // },
+  // {
+    
+  //   img: 'images/8.png'
+  // },
+  // {
+    
+  //   img: 'images/9.png'
+  // },
+  // {
+   
+  //   img: 'images/10.png'
+  // },
+// ];
 
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-}
+// console.log(cardArray);
+// cardArray.sort(() => 0.5 - Math.random());
+//create your board
 
-function unflipCards() {
-    boardLocked = true;
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        resetBoard();
-    },700);
-}
-
-function resetBoard() {
-    hasFlipCard = boardLocked = false;
-    firstCard = secondCard = null;
-}
-
-cards.forEach(card => {
-    card.addEventListener('click', flipCard);
-    const randomIndex = Math.floor(Math.random() * cards.length);
-    card.style.order = randomIndex;
-});
+  // for (let i = 0; i < cardsContent.length; i++) {
+  //   const card = document.createElement('img');
+  //   card.setAttribute('src', 'img/back.jpg');
+  //   card.setAttribute('data-id', i);
+  //   card.style.cssText = `width: 100px; height: 100px;  position: relative; display: flex; flex-direction: row; 
+  //   flex-wrap: wrap;align-items: center;justify-content: center; border: 3px solid #000; cursor: pointer;
+  //   user-select: none; margin: 3px; padding: 2px;`;
+  //   // card.addEventListener('click',flipCard);
+  //   field.appendChild(card);
+    
+  // }
 
 
+  
+
+  
 
 let gameField      = document.getElementById('field');
 let restartButton  = document.getElementById('restart');
 let stepsCounter   = document.getElementById('steps');
 let timerUI        = document.getElementById('timer');
 let cardViewInHTML = '<div class="card card_closed" data-status="closed"></div>';
-let cardsContent   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12];
+// let cardsContent   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12];
+let cardsContent   = ['img/1.png', 'img/2.png', 'img/3.png', 'img/4.png', 'img/5.png', 'img/6.png', 'img/7.png', 'img/8.png', 'img/9.png', 'img/10.png', 'img/11.png', 'img/12.png'];
+console.log(cardsContent);
 let openedCards    = [];
 let complitedCards = 0;
 let paused         = false;
@@ -207,3 +252,65 @@ function toggleTimer() {
   	timerUI.dataset.started = '';
   }
 }
+
+
+
+// two variant
+
+// const cards = document.querySelectorAll('.memo-card'); // Получаем массив элементов - карточек
+// let hasFlipCard = false; // Перевернута ли карточка
+// let boardLocked = false; //Запрет на кликание по другим карточкам пока открыты две одновременно
+// let firstCard; // сохроняем в переменнные DOM элементы для дальнейшнго сравнения
+// let secondCard;
+// console.log(cards);
+
+
+// const flipCard = (e) => {
+//     if(boardLocked) return;
+//     const target = e.target.parentElement;
+
+//     if(target === firstCard) return;  //если доска закрыта все действия прекращаются
+
+//     target.classList.add("flip");
+    
+//     if(!hasFlipCard) { //ПРоверяем какая была карточка
+//         //First click
+//         hasFlipCard = true;
+//         firstCard = target;
+//     }else{
+//         //Second click
+//         hasFlipCard = false;
+//         secondCard = target;
+//         checkForMatch(); // Проверка на совместимость, схожесть карточек (одинаковые ли)
+//     }
+// };
+// //Chek for match
+// function checkForMatch() {
+//     const isEqual = firstCard.dataset.ikon === secondCard.dataset.ikon;
+//     isEqual ? disableCards() : unflipCards(); // если тру остовляем карточки если false переворачиваем в исходное положение
+// }
+
+// function disableCards() {
+//     firstCard.removeEventListener('click', flipCard);
+//     secondCard.removeEventListener('click', flipCard);
+// }
+
+// function unflipCards() {
+//     boardLocked = true;
+//     setTimeout(() => {
+//         firstCard.classList.remove('flip');
+//         secondCard.classList.remove('flip');
+//         resetBoard();
+//     },700);
+// }
+
+// function resetBoard() {
+//     hasFlipCard = boardLocked = false;
+//     firstCard = secondCard = null;
+// }
+
+// cards.forEach(card => {
+//     card.addEventListener('click', flipCard);
+//     const randomIndex = Math.floor(Math.random() * cards.length);
+//     card.style.order = randomIndex;
+// });
